11622382fd0dfa098d26f4ed5c57497617dfb62a
Lnk User <lnk@localhost>
2025-11-09 09:57:24 +0000

    more fish stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,29 @@
 set fish_greeting ""
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    fastfetch || echo "[init] fastfetch not found."
     lnk pull >/dev/null || echo "[init] lnk is not installed."
 end
 
-setenv PATH "$PATH:/usr/local/go/bin"
-setenv PATH "$PATH:/root/.local/bin"
-setenv PATH "$PATH:$HOME/go/bin"
+setenv EDITOR flow
+
+# define an (initially empty) list of paths
+set paths_to_add
+
+# Local binaries
+set -a paths_to_add ~/.local/bin
+
+# Krew plugin manager
+set -a paths_to_add $HOME/.krew/bin
+
+# Go binaries
+set -a paths_to_add $HOME/go/bin
+
+# Go binaries
+set -a paths_to_add $HOME/.cargo/bin
+
+# Add each existing dir to PATH
+for dir in $paths_to_add
+    if test -d $dir
+        fish_add_path $dir
+    end
+end
